d76b0aafd395751143f5686b58d50d55576e75ad
Keval Solanki <57882119+keval8solanki@users.noreply.github.com>
2020-03-21 06:47:16 +0530

    Updates

---
+++ precache-manifest.353f18e0480c22bbc20ea63da1d1068f.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "8979716fd1a0ecf04f19",
+    "url": "/dashboard/static/css/main.641c2fce.chunk.css"
+  },
+  {
+    "revision": "8979716fd1a0ecf04f19",
+    "url": "/dashboard/static/js/main.74dcce31.chunk.js"
+  },
+  {
+    "revision": "dcfbc05562333eb34d98",
+    "url": "/dashboard/static/js/runtime~main.26f3d777.js"
+  },
+  {
+    "revision": "a5bd447c564aeb0ec397",
+    "url": "/dashboard/static/css/2.794ef97a.chunk.css"
+  },
+  {
+    "revision": "a5bd447c564aeb0ec397",
+    "url": "/dashboard/static/js/2.f84a620e.chunk.js"
+  },
+  {
+    "revision": "807999d233edcb68d6d50cc257be898f",
+    "url": "/dashboard/static/media/card-img.807999d2.jpeg"
+  },
+  {
+    "revision": "5bb294bac7185c4c103fc1938c4e1416",
+    "url": "/dashboard/static/media/close.5bb294ba.svg"
+  },
+  {
+    "revision": "142ecc2352ef0f9b00f8724fe0fb43b3",
+    "url": "/dashboard/index.html"
+  }
+];
